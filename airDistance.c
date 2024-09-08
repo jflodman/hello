3aac39f78e4c6a0e197f95477b192bfea6f785da
Joseph Flodman <jflodman3@huskers.unl.edu>
2024-09-08 22:45:51 +0000

    Sun, Sep 8, 2024, 5:45 PM -05:00

---
+++ airDistance.c
@@ -0,0 +1,9 @@
+/* Author: Joseph Flodman
+ *         jflodman3@huskers.unl.edu
+ * Date: 09/8/2024
+ *
+ * Write a program that *interactively prompts* the user to enter the latitude and
+longitude of two locations and then computes the distance between them
+using the above formula.
+ *
+ */
