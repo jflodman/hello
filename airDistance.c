e47fa3ef8896d70b9f5ce692f413b19ee3125190
Joseph Flodman <jflodman3@huskers.unl.edu>
2024-09-08 23:02:52 +0000

    Sun, Sep 8, 2024, 6:02 PM -05:00

--- airDistance.c
+++ airDistance.c
@@ -5,7 +5,7 @@
  *$$d = \arccos{(\sin(\varphi_1) \sin(\varphi_2) + \cos(\varphi_1) \cos(\varphi_2) \cos(\Delta) )} \cdot R$$
  *
  * $$r = \frac{deg}{180} \cdot \pi$$
- * 
+ *
  * Write a program that *interactively prompts* the user to enter the latitude and
 longitude of two locations and then computes the distance between them
 using the above formula.
@@ -17,7 +17,11 @@ using the above formula.
 #include <float.h>
 #include<math.h>
 
+#define PI 3.141592653589793
+#define EARTH_RADIUS 6371.0
+
 int main(int argc, char **argv) {
+
 
 double = $\varphi_1$
 double = $\varphi_2$
